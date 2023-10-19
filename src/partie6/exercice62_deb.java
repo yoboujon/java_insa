02a782ad1e2e87fac3d9103125a0fc20782be770
Yohan Boujon <yo.boujon@gmail.com>
2023-10-19 21:35:49 +0200

    Exercice 6, Partie 2.

---
+++ src/partie6/exercice62_deb.java
@@ -0,0 +1,11 @@
+package partie6;
+
+import com.itextpdf.text.Document;
+
+public class exercice62_deb {
+    public static void main(String[] args) {
+        Document document = new Document();
+        document.addAuthor("Yohan Boujon");
+        document.addCreationDate();
+    }
+}
